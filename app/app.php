77a067c4deeaddff30c925aa88814b64043b9cbf
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2019-09-09 21:31:43 -0400

    refactor: change api db config

--- app/app.php
+++ app/app.php
@@ -4,7 +4,7 @@ require __DIR__ . '../../vendor/autoload.php';
 
 $settings = require 'config/settings.php';
 
-$baseDir = __DIR__ . '../';
+$baseDir = __DIR__ . '/../';
 $envFile = $baseDir . '.env';
 if (file_exists($envFile)) {
     $dotenv = Dotenv\Dotenv::create($baseDir);
--- app/config/settings.php
+++ app/config/settings.php
@@ -25,7 +25,7 @@ return [
       'db' => [
         'driver' => 'mysql',
         'host' => getenv('DB_HOSTNAME'),
-        'port' => '3306',
+        'port' => getenv('DB_PORT'),
         'database' => getenv('DB_DATABASE'),
         'username' => getenv('DB_USERNAME'),
         'password' => getenv('DB_PASSWORD'),
